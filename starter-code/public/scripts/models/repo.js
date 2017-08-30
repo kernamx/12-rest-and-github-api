c316e17efa281e364c94e81eeb063b9315373ca7
Ron Barrantes <ronbarrantes@gmail.com>
2017-08-29 17:13:42 -0700

    project done

--- starter-code/public/scripts/models/repo.js
+++ starter-code/public/scripts/models/repo.js
@@ -1,16 +1,31 @@
 'use strict';
 var app = app || {};
 
+
+
 (function(module) {
   const repos = {};
 
   repos.all = [];
 
   repos.requestRepos = function(callback) {
-    // TODO: How would you like to fetch your repos? Don't forget to call the callback.
+    // DONE: How would you like to fetch your repos? Don't forget to call the callback.
     //       Remember that the callback function we'll want to call relies on repos.all
     //       being an array with a bunch of repo objects in it, so you'll need to
     //       populate it with the response from Github before you call the callback.
+
+    $.ajax({
+        url: 'https://api.github.com/user/repos',
+        method: 'GET',
+        headers: {
+          Authorization: `token ${githubToken}`
+        }
+      })
+      .then(
+        data => data.forEach(repo => repos.all.push(repo),
+        err => console.error(err)
+      ))
+      .then(callback);
 
   };
 
